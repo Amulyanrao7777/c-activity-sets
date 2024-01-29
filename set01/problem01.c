15bfe555f2f553c0e23234d54a4163ee563d6aed
Amulya N <amulyan.bsc23@rvu.edu.in>
2024-01-29 11:01:33 +0530

    test

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,1 +1,8 @@
 // 1. Write a C program to print your name.
+
+#include <stdio.h>
+int main()
+{
+    printf("\n Amulya \n");
+    return 0;
+}
--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,9 @@
 //2. Write a C program to add two numbers.
-
+#include <stdio.h>
+int main()
+{
+    int x=3, y=5, sum = 0;
+    sum = x+y;
+    printf("The sum of %d and %d is %d \n", x,y, sum);
+    return 0;
+}
