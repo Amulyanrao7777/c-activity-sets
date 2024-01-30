b71b630fd77510c7a797b23ba9efa3205a95f2e6
Amulya N <amulyan.bsc23@rvu.edu.in>
2024-01-30 09:40:31 +0530

    committed 1/30/2024, 9:40:31 AM

---
+++ set01/problem07.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int input_n()
+{   int x;
+    printf("Enter n \n");
+    scanf("%d", &x);
+    return x;
+}
+int sum_n_nos(int n)
+{
+    int i, sum=0;
+    for(i=0;i<=n;i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+
+}
+void output(int n, int sum)
+{
+    printf("The sum of all natural numbers until %d is %d", n, sum);
+
+}
+int main()
+{
+    int n, sum;
+    n = input_n();
+    sum = sum_n_nos(n);
+    output(n, sum);
+
+}
