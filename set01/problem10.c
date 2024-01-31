44cb97f04eea3455e8ccf1bae188bee6bf814fe9
Amulya N <amulyan.bsc23@rvu.edu.in>
2024-01-31 20:17:57 +0530

    committed 1/31/2024, 8:17:57 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,4 +1,44 @@
 #include <stdio.h>
-void input_two_strings(char *string1, char *string2);
-int stringcompare(char *string1, char *string2);
-void output(char *string1, char *string2, int result);
+void input_two_strings(char *string1, char *string2)
+{
+    printf("Enter the string 1 \n");
+    scanf("%s", string1);
+    printf("Enter the string 2 \n");
+    scanf("%s", string2);
+
+}
+
+int stringcompare(char *string1, char *string2)
+{
+    int n=0;
+    int i;
+    for(i=0; string1[i]== string2[i] && string1[i]!='\0'; i++);
+    n = string1[i] - string2[i];
+    return n;
+}
+void output(char *string1, char *string2, int result)
+{
+    if(result>0)
+    {
+        printf("%s is greater than %s", string1, string2);
+    }
+    else if(result<0)
+    {   
+        printf("%s is greater than %s", string2, string1);
+
+    }
+    else 
+    {
+        printf("%s is equal to %s", string1, string2);
+    }
+}
+
+int main()
+{
+    int result;
+    char string1[100], string2[100];
+    input_two_strings(string1, string2);
+    result = stringcompare(string1, string2);
+    output(string1,string2, result);
+
+}
--- set01/problem11.c
+++ set01/problem11.c
@@ -2,9 +2,36 @@
 struct _complex {
 	float real;
 	float imaginary;
-}
+};
 typedef struct _complex Complex;
 
-Complex input_complex();
-Complex add_complex(Complex a, Complex b);
-void output(Complex a, Complex b, Complex sum);
+Complex input_complex()
+{ 
+  Complex c1;
+  printf("Enter the real part of complex number\n");
+  scanf("%f", &c1.real);
+  printf("Enter the imaginary part of complex number\n");
+  scanf("%f", &c1.imaginary);
+  return c1;
+}
+Complex add_complex(Complex a, Complex b)
+{
+  Complex sum;
+  sum.imaginary= a.imaginary+b.imaginary;
+  sum.real= a.real+b.real;
+  return sum;
+}
+void output(Complex a, Complex b, Complex sum)
+{
+	printf("The sum of %f + %fi and %f + %fi is %f + %fi", a.real, a.imaginary,b.real,b.imaginary,sum.real,sum.imaginary);
+
+}
+
+ int main()
+ {
+	Complex a, b, sum;
+	a= input_complex();
+	b= input_complex();
+	sum = add_complex(a,b);
+	output(a,b,sum);
+ }
