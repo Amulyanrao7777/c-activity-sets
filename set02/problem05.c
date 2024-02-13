ef2b2d32f0e601c108f3345fd88191140282a985
Amulya N <amulyan.bsc23@rvu.edu.in>
2024-02-13 13:31:28 +0530

    committed 2/13/2024, 1:31:27 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
-int input();
-int find_gcd(int a, int b);
-void output(int a, int b, int gcd);
+int input()
+{   
+    int x;
+    
+    scanf("%d", &x);
+    return x;
+}
+int find_gcd(int a, int b)
+{   
+    int temp;
+    
+    while(b!=0)
+    {
+        temp = b;
+        b = a%b;
+        a= temp;
+
+    }
+    return a;
+}
+void output(int a, int b, int gcd)
+{
+    printf("The gcd of %d and %d is %d", a,b,gcd);
+}
+int main()
+{
+    int a, b, gcd;
+    a= input();
+    b= input();
+    gcd = find_gcd(a,b);
+    output(a,b,gcd);
+}
