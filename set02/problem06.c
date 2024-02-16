fd36fadedc425b7908fd2162dc62988d7ce93ddd
Amulya N <amulyan.bsc23@rvu.edu.in>
2024-02-16 16:03:37 +0530

    committed 2/16/2024, 4:03:37 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
-void input_string(char *a);
-void str_reverse(char *str, char *rev_str);
-void output(char *a, char *reverse_a);
+void input_string(char *a)
+{
+    printf("Enter the string \n");
+    scanf("%s", a);
+}
+void str_reverse(char *str, char *rev_str)
+{   int i;
+    for(i=0; str[i]!= '\0'; i++)
+    {
+        for(int j=0; str[j]!= '\0'; j++)
+        {
+            rev_str[j] = str[i];
+        }
+    }
+}
+void output(char *a, char *reverse_a)
+{
+    printf("%s backwards is %s", a, reverse_a);
+}
+
+
+int main()
+{
+    char str[100], rev_str[100];
+    input_string(str);
+    str_reverse(str, rev_str);
+    output(str, rev_str);
+}
