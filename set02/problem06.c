5fdb7a32321ef6e353685ee8608a400a3551037b
Amulya N <amulyan.bsc23@rvu.edu.in>
2024-02-27 11:27:39 +0530

    committed 2/27/2024, 11:27:39 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -6,17 +6,23 @@ void input_string(char *a)
 }
 void str_reverse(char *str, char *rev_str)
 {   int i;
-    for(i=0; str[i]!= '\0'; i++)
+    int a;
+    for(a=0; str[a]!= '\0'; a++);
+
+    for(i=0; i<a; i++)
+    
     {
-        for(int j=0; str[j]!= '\0'; j++)
-        {
-            rev_str[j] = str[i];
-        }
+
+    rev_str[i] = str[a-i-1];
+
     }
+    rev_str[a] = '\0';
+
 }
 void output(char *a, char *reverse_a)
 {
-    printf("%s backwards is %s", a, reverse_a);
+    printf("The original string is %s \n", a);
+    printf("The reversed string is %s \n", reverse_a);
 }
 
 
@@ -25,5 +31,7 @@ int main()
     char str[100], rev_str[100];
     input_string(str);
     str_reverse(str, rev_str);
+
     output(str, rev_str);
+    return 0;
 }
---
+++ set02/problem07.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+struct _triangle {
+	float base, altitude, area;
+};
+typedef struct _triangle Triangle;
+
+Triangle input_triangle()
+{   Triangle T1;
+    printf("enter the base and altitude of the triangle \n");
+    scanf("%d",&T1.base,&T1.altitude );
+    return T1;
+}
+void find_area(Triangle *t)
+{
+    t->area = (0.5)*t->altitude*t->base;
+}
+
+void output(Triangle t)
+{
+    printf("The area of Triangle with base %f and height %f is %f",t.base, t.altitude, t.area);
+}
+int main()
+{
+    Triangle t;
+    t = input_triangle();
+    find_area(&t);
+    output(t);
+}
